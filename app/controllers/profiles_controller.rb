f4fd3445e81a24c14bd91fd39d4ff4371bf040e1
Adam White <adjackwhite@gmail.com>
2020-12-01 11:04:11 +0800

    removed skills tags on params

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,6 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile).permit(:location, :skill, :rate, :description, :location_specific)
+    params.require(:profile).permit(:location, :rate, :description, :location_specific)
   end
 end
