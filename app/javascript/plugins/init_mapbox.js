752a290579ceeb44391d75813aa6a26fae57f944
Elizabeth Giraldo <elizabethgiraldopinto@gmail.com>
2020-12-02 10:45:14 +0800

    map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,6 +4,8 @@ const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+}
+
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -15,14 +17,11 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
-    });
+    addMarkersToMap(map, markers)
     fitMapToMarkers(map, markers);
   }
 };
+
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
