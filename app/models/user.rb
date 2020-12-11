392ad272b1210385f6ffdfd02ba9c7a0bb3178a5
Elizabeth Giraldo <elizabethgiraldopinto@gmail.com>
2020-12-11 10:57:46 +0800

    add pic to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_one :profile
+  has_one_attached :image
   has_many :conversations
   validates :email, presence: true
 end
