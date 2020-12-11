86e849b96b7ee7277cc02dfec696d6033d94405b
Elizabeth Giraldo <elizabethgiraldopinto@gmail.com>
2020-12-11 13:53:15 +0800

    ratings on profiles

--- app/models/freelancer_review.rb
+++ app/models/freelancer_review.rb
@@ -1,3 +1,12 @@
 class FreelancerReview < ApplicationRecord
   belongs_to :booking
+  has_one :profile, through: :booking
+  after_create :update_profile_rating
+
+  def update_profile_rating
+    reviews = self.profile.freelancer_reviews
+    average = reviews.pluck(:rating).sum  / reviews.size
+    self.profile.update(rating: average)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_025145) do
+ActiveRecord::Schema.define(version: 2020_12_11_050436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_025145) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.float "rating"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
