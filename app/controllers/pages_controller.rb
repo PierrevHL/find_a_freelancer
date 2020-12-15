fbb1f5119f25ea529deed1a28008a0467030de47
Pierre Habsburg <pierre.habsburg@googlemail.com>
2020-12-15 10:52:03 +0100

    SortedBookingConfirmation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,12 +10,15 @@ class PagesController < ApplicationController
 
 
   def dashboard
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.order(:start_date)
     update_bookings(@bookings)
-  
     
-    @my_bookings = Booking.where(profile: current_user.profile)
-    @freelancer_bookings = Booking.joins(profile_skill: :profile).where(profiles: {user_id: current_user.id}).where.not(status: "rejected")
+    
+    @my_bookings = Booking.where(profile: current_user.profile).order(:start_date)
+    @freelancer_bookings = Booking.joins(profile_skill: :profile).where(profiles: {user_id: current_user.id}).where.not(status: "rejected").order(:start_date)
+      @bookings = @bookings.where(status: params[:status]) if params[:status].present?
+      @freelancer_bookings = @freelancer_bookings.where(status: params[:status]) if params[:status].present?
+
   end
 
   private
